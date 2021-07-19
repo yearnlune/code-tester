325d2cdfc4c014860e1d472df0f0254cb533b502
DongHwan Kim <kdhpopoya@gmail.com>
2021-07-20 06:49:55 +0900

    feat: Added simple solution to 'groupBy'

--- resources/sql/groupBy.sql
+++ resources/sql/groupBy.sql
@@ -1,5 +1,11 @@
 /* https://programmers.co.kr/learn/courses/30/lessons/59040 */
 
+SELECT ANIMAL_TYPE,
+       COUNT(1) as 'count'
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE;
+
 SELECT *
 FROM (SELECT ANIMAL_TYPE,
              COUNT(1) as 'count'
